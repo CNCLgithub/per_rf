687ea2729dfeae4762212b1b9124e49e3a59482d
belledon <mbelledonne@gmail.com>
2019-09-05 10:26:11 -0400

    fixed cors request using http-server

--- experiment.js
+++ experiment.js
@@ -118,25 +118,6 @@ var trialTemplate = new lab.flow.Sequence({
 })
 
 // Define the trials in terms of the central parameters:
-// The word shown on screen, and its color
-// var trials = [
-//   { color: 'red', word: 'red' },
-//   { color: 'red', word: 'green' },
-//   { color: 'red', word: 'blue' },
-//   { color: 'red', word: 'orange' },
-//   { color: 'green', word: 'red' },
-//   { color: 'green', word: 'green' },
-//   { color: 'green', word: 'blue' },
-//   { color: 'green', word: 'orange' },
-//   { color: 'blue', word: 'red' },
-//   { color: 'blue', word: 'green' },
-//   { color: 'blue', word: 'blue' },
-//   { color: 'blue', word: 'orange' },
-//   { color: 'orange', word: 'red' },
-//   { color: 'orange', word: 'green' },
-//   { color: 'orange', word: 'blue' },
-//   { color: 'orange', word: 'orange' },
-// ]
 var trials = [
     {
         first: 'media/test_image_1.png',
@@ -154,56 +135,53 @@ var experiment = new lab.flow.Sequence({
   content: [
     // Initial instructions
       new lab.html.Screen({
-          content: 'Hello world!'
-      }),
-      new lab.html.Screen({
           contentUrl: 'pages/1-welcome.html',
           responses: {
               'keypress(Space)': 'continue'
           },
       }),
-  //   // Instruction summary
-  //   new lab.html.Screen({
-  //     contentUrl: 'pages/2-summary.html',
-  //     responses: {
-  //       'keypress(Space)': 'continue'
-  //     },
-  //   }),
-  //   // Practice trials
-  //   new lab.flow.Loop({
-  //     template: trialTemplate,
-  //     templateParameters: trials,
-  //     shuffle: true,
-  //     parameters: {
-  //       feedback: true,
-  //     },
-  //   }),
-  //   // Interlude
-  //   new lab.html.Screen({
-  //     contentUrl: 'pages/4-interlude.html',
-  //     responses: {
-  //       'keypress(Space)': 'continue',
-  //     },
-  //   }),
-  //   // Actual trials
-  //   new lab.flow.Loop({
-  //     template: trialTemplate,
-  //     templateParameters: trials,
-  //     shuffle: true,
-  //     parameters: {
-  //       feedback: false,
-  //     },
-  //   }),
-  //   // Thank-you page
-  //   new lab.html.Screen({
-  //     contentUrl: 'pages/5-thanks.html',
-  //     // Respond to clicks on the download button
-  //     events: {
-  //       'click button#download': function() {
-  //         this.options.datastore.download()
-  //       },
-  //     },
-  //   }),
+    // Instruction summary
+    new lab.html.Screen({
+      contentUrl: 'pages/2-summary.html',
+      responses: {
+        'keypress(Space)': 'continue'
+      },
+    }),
+    // Practice trials
+    new lab.flow.Loop({
+      template: trialTemplate,
+      templateParameters: trials,
+      shuffle: true,
+      parameters: {
+        feedback: true,
+      },
+    }),
+    // Interlude
+    new lab.html.Screen({
+      contentUrl: 'pages/4-interlude.html',
+      responses: {
+        'keypress(Space)': 'continue',
+      },
+    }),
+    // Actual trials
+    new lab.flow.Loop({
+      template: trialTemplate,
+      templateParameters: trials,
+      shuffle: true,
+      parameters: {
+        feedback: false,
+      },
+    }),
+    // Thank-you page
+    new lab.html.Screen({
+      contentUrl: 'pages/5-thanks.html',
+      // Respond to clicks on the download button
+      events: {
+        'click button#download': function() {
+          this.options.datastore.download()
+        },
+      },
+    }),
   ],
 })
 // Collect data in a central data store
