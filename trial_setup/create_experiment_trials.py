3dac760724043708ffd7a6484fd955d8cb7ae138
Hakan Yilmaz <hakany@mit.edu>
2019-09-13 16:10:33 -0400

    trials now saved in .js file

--- trial_setup/create_experiment_trials.py
+++ trial_setup/create_experiment_trials.py
@@ -10,22 +10,22 @@ flipped = [False, True]
 thirds = ['first', 'second']
 
 # create the trial list
-data = {'trials': []}
+data = []
 
 for img in imgs:
     for scale in scales:
         for flip in flipped:
             for third in thirds:
                 if not flip:
-                    data['trials'].append({'first': join(ref_dir, '{:02d}.png'.format(img)),
+                    data.append({'first': join(ref_dir, '{:02d}.png'.format(img)),
                                            'second': join(metamer_dir,
                                                           '{:02d}_s{:.1f}_a2_o0.5_iter_50.png'.format(img, scale)),
                                            'third': third})
                 else:
-                    data['trials'].append({'first': join(metamer_dir,
+                    data.append({'first': join(metamer_dir,
                                                          '{:02d}_s{:.1f}_a2_o0.5_iter_50.png'.format(img, scale)),
                                            'second': join(ref_dir, '{:02d}.png'.format(img)),
                                            'third': third})
 
-with open('experiment_trials.json', 'w') as outfile:
-    json.dump(data, outfile)
+with open('experiment_trials.js', 'w') as outfile:
+    outfile.write('var EXPERIMENT_TRIALS =\n'+json.dumps(data, indent=4))
--- trial_setup/create_practice_trials.py
+++ trial_setup/create_practice_trials.py
@@ -9,18 +9,18 @@ scale_pairs = [[0.3, 0.4],  # 0 means original
                [0.3, 0.5]]
 
 # create the trial list
-data = {'trials': []}
+data = []
 
 for img in imgs:
     for scale_pair in scale_pairs:
         shuffle(scale_pair)
-        if choice([True,False]):
+        if choice([True, False]):
             third = 'first'
         else:
             third = 'second'
-        data['trials'].append({'first': join(img_source_dir, 'metamer_imgs/{:02d}_s{:.1f}_a2_o0.5_iter_50.png'.format(img, scale_pair[0])),
-                               'second': join(img_source_dir, 'metamer_imgs/{:02d}_s{:.1f}_a2_o0.5_iter_50.png'.format(img, scale_pair[1])),
-                               'third': third})
+        data.append({'first': join(img_source_dir, 'metamer_imgs/{:02d}_s{:.1f}_a2_o0.5_iter_50.png'.format(img, scale_pair[0])),
+                     'second': join(img_source_dir, 'metamer_imgs/{:02d}_s{:.1f}_a2_o0.5_iter_50.png'.format(img, scale_pair[1])),
+                     'third': third})
 
-with open('practice_trials.json', 'w') as outfile:
-    json.dump(data, outfile)
+with open('practice_trials.js', 'w') as outfile:
+    outfile.write('var PRACTICE_TRIALS =\n'+json.dumps(data, indent=4))
