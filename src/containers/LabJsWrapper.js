c79efbc3b40ea1d2c5dc5d129b4b6babcb3685bf
belledon <mbelledonne@gmail.com>
2019-10-10 21:24:23 -0400

    npm builds

--- src/containers/LabJsWrapper.js
+++ src/containers/LabJsWrapper.js
@@ -9,27 +9,6 @@ const config = require('../config');
 var _ = require('lodash');
 var qs = require('query-string');
 
-// // Import questlib
-// require('questlib');
-
-// const questParamsToKeep = [
-//   'updatePdf',
-//   'warnPdf',
-//   'normalizePdf',
-//   'tGuess',
-//   'tGuessSd',
-//   'pThreshold',
-//   'xThreshold',
-//   'beta',
-//   'delta',
-//   'gamma',
-//   'grain',
-//   'dim',
-//   'quantileOrder',
-//   'intensity',
-//   'response',
-// ];
-
 
 class LabJsWrapper extends Component {
   constructor(props) {
@@ -54,9 +33,9 @@ class LabJsWrapper extends Component {
           this.addScript(process.env.PUBLIC_URL + '/lib/lab.js', () => {
               // If we add this script tag before lab.js loads, then the
               // script will not be able to find the lab module.
-              this.addScript(process.env.PUBLIC_URL + '/lib/jquery-3.4.1.min.js'),
-              this.addScript(process.env.PUBLIC_URL + '/trial_setup/practice_trials.js'),
-              this.addScript(process.env.PUBLIC_URL + '/trial_setup/experiment_trials.js'),
+              this.addScript(process.env.PUBLIC_URL + '/lib/jquery-3.4.1.min.js');
+              this.addScript(process.env.PUBLIC_URL + '/trial_setup/practice_trials.js');
+              this.addScript(process.env.PUBLIC_URL + '/trial_setup/experiment_trials.js');
               this.addScript(process.env.PUBLIC_URL + '/script.js')
           });
       }
@@ -144,36 +123,25 @@ class LabJsWrapper extends Component {
       window.location.assign(this.state.link);
     }
 
-    return (
-            <div id="dpi" style="height: 1in; width: 1in; left: 100%; position: fixed; top: 100%;"></div>
-            <div id="experiment" data-labjs-section="main" class="container fullscreen" style={{visibility: this.state.sendingData ? 'hidden' : 'visible'}}>
-            <main class="content-vertical-center content-horizontal-center">
-            <div>
-            <h2>Loading Experiment</h2>
-            <p>The experiment is loading and should start in a few seconds</p>
-            </div>
-            </main>
-            <footer class="content-vertical-center content-horizontal-center">
-            </footer>
-            <div className="center" style={{visibility: this.state.sendingData ? 'visible' : 'hidden'}}>
-            <h2>Saving data... do not exit window</h2>
-            </div>
+      return (
+              <div>
+                  <div id="dpi" style="height: 1in; width: 1in; left: 100%; position: fixed; top: 100%;"></div>
+                  <div id="experiment" data-labjs-section="main" class="container fullscreen" style={{visibility: this.state.sendingData ? 'hidden' : 'visible'}}>
+                  <main class="content-vertical-center content-horizontal-center">
+                      <div>
+                          <h2>Loading Experiment</h2>
+                          <p>The experiment is loading and should start in a few seconds</p>
+                      </div>
+                  </main>
+                  <footer class="content-vertical-center content-horizontal-center">
+                  </footer>
+                      <div className="center" style={{visibility: this.state.sendingData ? 'visible' : 'hidden'}}>
+                           <h2>Saving data... do not exit window</h2>
+                      </div>
+                  </div>
+              </div>
     );
   } // end render
 } // end class
 
 export default LabJsWrapper;
-
-      // <div>
-      //   <div className="container fullscreen" data-labjs-section="main" style={{visibility: this.state.sendingData ? 'hidden' : 'visible'}}>
-      //     <main className="content-vertical-center content-horizontal-center">
-      //       <div>
-      //         <h2>Loading Experiment</h2>
-      //         <p>The experiment is loading and should start in a few seconds</p>
-      //       </div>
-      //     </main>
-      //   </div>
-      //   <div className="center" style={{visibility: this.state.sendingData ? 'visible' : 'hidden'}}>
-      //     <h2>Saving data... do not exit window</h2>
-      //   </div>
-      // </div>
